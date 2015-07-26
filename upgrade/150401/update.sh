ad973bff5e23bb64c6a1e3b7148e0d1e3ff83577
Nico Isenbeck <nico.isenbeck@web.de>
2015-07-26 12:32:06 +0200

    Debian Jessie upgrade instructions

--- upgrade/150401/update.sh
+++ upgrade/150401/update.sh
@@ -22,9 +22,45 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
+DEBIAN_VERSION=$(cut -d"." -f1 /etc/debian_version)
+
+if [ "$DEBIAN_VERSION" = "7" ]; then {
+echo -e "
+
+---------------------------------------------------------------------------------
+\033[40;37mYou are using Bananian $READABLE_VERSION on Debian Wheezy\033[0m
+---------------------------------------------------------------------------------
+
+\033[40;33mBananian 15.04 was the last release based on Debian 7/Wheezy.\033[0m
+
+Now you have two options:
+-------------------------
+1.) Upgrade to Debian 8/Jessie
+2.) Stay at Debian 7/Wheezy
+
+Bananian 15.08 (and later versions) will only be available for Debian 8/Jessie.
+Debian 7/Wheezy is still supported and will receive kernel and security updates.
+
+Upgrade instructions:
+---------------------
+1.) sed -i 's/wheezy/jessie/g' /etc/apt/sources.list
+2.) apt-get update
+3.) apt-get upgrade
+4.) apt-get dist-upgrade
+5.) shutdown -r now
+
+More detailled instructions on debian.org:
+https://www.debian.org/releases/jessie/armhf/release-notes/ch-upgrading.en.html
+
+"
+
+} else {
+
 echo -e "---------------------------------------------------------------------------------"
 echo -e "
 \033[32;40m You are already using Bananian $READABLE_VERSION \033[0m
 
 No upgrades available...
 "
+
+} fi
